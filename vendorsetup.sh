b5fa08a7dabc3cd3f59ead4b31f3d8e4a1bfde61
S ∆ U R ∆ V <dwarmachine24@gmail.com>
2022-12-17 15:48:43 +0530

    crownlte: Update vendorsetup.sh repo name

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,16 @@
 rm -rf device/samsung/exynos9810-common
-git clone https://github.com/ProjectElixir-Devices/device_samsung_exynos9810-common device/samsung/exynos9810-common
+git clone https://github.com/ProjectElixir-Devices/device_samsung_exynos9810-common -b v2 device/samsung/exynos9810-common
 
 rm -rf vendor/samsung/exynos9810-common
-git clone --depth=1 https://github.com/samsungexynos9810/vendor_samsung_exynos9810-common vendor/samsung/exynos9810-common
+git clone --depth=1 https://github.com/samsungexynos9810/vendor_samsung_exynos9810-common-V2 vendor/samsung/exynos9810-common
 
 rm -rf vendor/samsung/crownlte
-git clone --depth=1 https://github.com/samsungexynos9810/vendor_samsung_crownlte vendor/samsung/crownlte
+git clone --depth=1 https://github.com/samsungexynos9810/vendor_samsung_crownlte-V2 vendor/samsung/crownlte
 
 rm -rf device/samsung_slsi/sepolicy
 git clone https://github.com/samsungexynos9810/device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy
 
 rm -rf kernel/samsung/exynos9810
-git clone --depth=1 https://github.com/samsungexynos9810/kernel_samsung_exynos9810 kernel/samsung/exynos9810
+git clone --depth=1 https://github.com/samsungexynos9810/kernel_samsung_exynos9810-V2 kernel/samsung/exynos9810
 
 
